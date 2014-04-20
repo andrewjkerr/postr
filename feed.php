fd9f8ed29a706c6a73a83600418e41040194ee67
Andrew Kerr <andrewjkerr47@gmail.com>
2014-04-20 14:35:10 -0400

    Added dynamic followers/following counts

--- feed.php
+++ feed.php
@@ -1,4 +1,5 @@
 <?php session_start(); 
+include('connect.php');
 if(empty($_SESSION['username'])){
 	echo '<META HTTP-EQUIV="Refresh" Content="1; URL=login.html#error1">';
 	exit;
@@ -28,9 +29,21 @@ if(empty($_SESSION['username'])){
 		<div id="feed_top">
 			<div class="row">
 				<div class="floatedCellUsername" style="font-size: 24px">@<?php echo $_SESSION['username'] ?></div>
-				<div class="floatedCellFollowers" style="font-size: 24px">Followers: 100</div>
-				<div class="floatedCellFollowing" style="font-size: 24px">Following: 20</div>
-				<div class="floatedCellNewPost"><a class="myButton" style="width: 80px; float:right; margin-top: -5px">NEW POST</a></div>
+				<?php
+				$uid = $_SESSION['uid'];
+				// Query DB and fetch result
+				$query = pg_prepare($conn, "numfollowers", 'SELECT * FROM follows WHERE follows.follow_uid = $1');
+				$result = pg_execute($conn, "numfollowers", array($uid));
+				$arr = pg_fetch_all($result);
+				$numFollowers = count($arr);
+				echo '<div class="floatedCellFollowers" style="font-size: 24px">Followers: ' . $numFollowers . '</div>';
+				$query = pg_prepare($conn, "numfollowing", 'SELECT * FROM follows WHERE follows.follower_uid = $1');
+				$result = pg_execute($conn, "numfollowing", array($uid));
+				$arr = pg_fetch_all($result);
+				$numFollowing = count($arr);
+				echo '<div class="floatedCellFollowing" style="font-size: 24px">Following: ' . $numFollowing . '</div>';	
+				?>
+				<div class="floatedCellNewPost"><a href="new-post.php" class="myButton" style="width: 80px; float:right; margin-top: -5px">NEW POST</a></div>
 				<div class="clear"></div>
 			</div>
 		</div>
