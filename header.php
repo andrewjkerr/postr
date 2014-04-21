0a3f200f7dd5e21e960d13ec18dd807a50f1b5ee
Andrew Kerr <andrewjkerr47@gmail.com>
2014-04-21 02:17:26 -0400

    Added some logic into the header

--- header.php
+++ header.php
@@ -3,6 +3,14 @@
 		<a href="feed.php" style="color:#fff">postr</a>
 	</div>
 	<div id = "menu">
-		Hi <?php echo '<a href="post.php?username=' . $_SESSION['username'] . '">@' . $_SESSION['username'] . '</a>!' ?> | <a href="feed.php">Home</a> | <a href="about.php">About</a> | <a href="search.php">Search</a> | <a href="logout.php">Logout</a>
+		<?php
+			if(isset($_SESSION['username'])){
+				echo 'Hi <a href="post.php?username=' . $_SESSION['username'] . '">@' . $_SESSION['username'] . '</a>!';
+			}
+			else{
+				echo 'Hi Guest! You should <a href="index.html">register</a> or <a href="login.html">login!</a>';
+			}
+		?>
+		| <a href="feed.php">Home</a> | <a href="about.php">About</a> | <a href="search.php">Search</a> | <a href="logout.php">Logout</a>
 	</div>
 </div>
