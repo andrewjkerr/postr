fa92adb68070a6ec344da217cc0ae1b0a5e84d1c
Andrew Kerr <andrewjkerr47@gmail.com>
2014-04-20 20:01:00 -0400

    Fixes #44

--- feed.php
+++ feed.php
@@ -35,12 +35,12 @@ if(empty($_SESSION['username'])){
 				$query = pg_prepare($conn, "numfollowers", 'SELECT * FROM follows WHERE follows.follow_uid = $1');
 				$result = pg_execute($conn, "numfollowers", array($uid));
 				$arr = pg_fetch_all($result);
-				$numFollowers = count($arr);
+				$numFollowers = pg_num_rows($result);
 				echo '<div class="floatedCellFollowers" style="font-size: 24px">Followers: ' . $numFollowers . '</div>';
 				$query = pg_prepare($conn, "numfollowing", 'SELECT * FROM follows WHERE follows.follower_uid = $1');
 				$result = pg_execute($conn, "numfollowing", array($uid));
 				$arr = pg_fetch_all($result);
-				$numFollowing = count($arr);
+				$numFollowing = pg_num_rows($result);
 				echo '<div class="floatedCellFollowing" style="font-size: 24px">Following: ' . $numFollowing . '</div>';	
 				?>
 				<div class="floatedCellNewPost"><a href="new-post.php" class="myButton" style="width: 80px; float:right; margin-top: -5px">NEW POST</a></div>
@@ -53,39 +53,45 @@ if(empty($_SESSION['username'])){
 			<?php
 				$url = "http://akerr.me/postr/get-feed.php?uid=" . $_SESSION['uid'];
 				$json = file_get_contents($url);
-				$obj = json_decode($json);
-				if(isset($_GET['page'])) {
-					$page = $_GET['page'];
-					$page = $page * 10;
-					if($page + 10 < count($obj)){
-						$end = count($obj) % $page;
-					}
-					else{
-						$end = $page + 10;
-					}
+				if(strcmp($json, "noposts") == 0){
+					echo '<h2 style="text-align: center">You are not following anyone, so follow our founders!</h2>';
+					echo '<div id="founder_list"><a href="post.php?username=andrew">@andrew</a> | <a href="post.php?username=andres">@andres</a> | <a href="post.php?username=omeed">@omeed</a> | <a href="post.php?username=samuel">@samuel</a></div>';
 				}
-				else {
-					$page = 0;
-					$end = count($obj);
-				}
-				for($i = $page; $i < $end; $i++){
-					echo '<div class="row">
-						<div class="floatedCellUsername"><a href="post.php?username='. $obj[$i]->username . '">@' . $obj[$i]->username . '</a></div>';
-					if($obj[$i]->content_type == 1) {
-						echo '<div class="floatedCellPost"><img src="' . $obj[$i]->content . '" style="width: 250px; height: 250px" /></div>';
+				else{
+					$obj = json_decode($json);
+					if(isset($_GET['page'])) {
+						$page = $_GET['page'];
+						$page = $page * 10;
+						if($page + 10 < count($obj)){
+							$end = count($obj) % $page;
+						}
+						else{
+							$end = $page + 10;
+						}
 					}
-					else{
-						echo '<div class="floatedCellPost">' . $obj[$i]->content . '</div>';
+					else {
+						$page = 0;
+						$end = count($obj);
 					}
-					echo '<div class="clear"></div>
-					</div>
-					<div class="button_row">
-						<div class="floatedCellUsername"></div>
-						<div class="buttons" style="margin-left: 290px">
-							<a href="like.php?pid=' . $obj[$i]->pid . '&uid1=' . $_SESSION['uid'] . '" class="myButton" style="width: 30px; padding: 5px 10px">LIKE</a>
-							<a href="post.php?username=' . $obj[$i]->username . '"class="myButton" style="width: 100px; margin-left: 15px; padding: 5px 10px">COMMENT</a>
+					for($i = $page; $i < $end; $i++){
+						echo '<div class="row">
+							<div class="floatedCellUsername"><a href="post.php?username='. $obj[$i]->username . '">@' . $obj[$i]->username . '</a></div>';
+						if($obj[$i]->content_type == 1) {
+							echo '<div class="floatedCellPost"><img src="' . $obj[$i]->content . '" style="width: 250px; height: 250px" /></div>';
+						}
+						else{
+							echo '<div class="floatedCellPost">' . $obj[$i]->content . '</div>';
+						}
+						echo '<div class="clear"></div>
 						</div>
-					</div>';
+						<div class="button_row">
+							<div class="floatedCellUsername"></div>
+							<div class="buttons" style="margin-left: 290px">
+								<a href="like.php?pid=' . $obj[$i]->pid . '&uid1=' . $_SESSION['uid'] . '" class="myButton" style="width: 30px; padding: 5px 10px">LIKE</a>
+								<a href="post.php?username=' . $obj[$i]->username . '"class="myButton" style="width: 100px; margin-left: 15px; padding: 5px 10px">COMMENT</a>
+							</div>
+						</div>';
+					}
 				}
 			?>
 		</div>
