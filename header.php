887cb2ce85970fe5cbdf5649adf6e4a05eb1baa2
Andrew Kerr <andrewjkerr47@gmail.com>
2014-04-20 23:49:46 -0400

    Fixes #47

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <div id = "header">
 	<div id = "logo">
-		postr
+		<a href="feed.php" style="color:#fff">postr</a>
 	</div>
 	<div id = "menu">
-		<a href="feed.php">Home</a> | <a href="about.php">About</a> | <a href="search.php">Search</a> | <a href="logout.php">Logout</a>
+		Hi <?php echo '<a href="post.php?username=' . $_SESSION['username'] . '">@' . $_SESSION['username'] . '</a>!' ?> | <a href="feed.php">Home</a> | <a href="about.php">About</a> | <a href="search.php">Search</a> | <a href="logout.php">Logout</a>
 	</div>
 </div>
